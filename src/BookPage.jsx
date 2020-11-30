55d7464178f94577fedabb457d5311521a9a9f52
ParkDongJo <pdj10211@gmail.com>
2020-12-01 00:18:57 +0900

    [etc. PR] 코드 충돌 해결

--- src/BookPage.jsx
+++ src/BookPage.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import styled from '@emotion/styled';
 
-import BookPage from './BookPage';
+import PageImage from './PageImage';
 
 export default function BookPage() {
   return (
-    <Layout data-testid="lectures-layout">
-      <h1>Lectures</h1>
+    <Layout data-testid="book-layout">
+      <h1>Book</h1>
       <PageImage
         width={'100%'}
         imageUrl={'https://webdev.imgix.net/web-bundles/hero.png?auto=format&fit=max&w=1730'}
