f6687956cd1ff665cae5290dba257d554c95a48c
ParkDongJo <pdj10211@gmail.com>
2020-12-02 16:59:50 +0900

    [week3-task1] 가상 데이터 fixtures 에 courses, page 데이터셋 추가

---
+++ fixtures/courses.js
@@ -0,0 +1,24 @@
+// ~/books/1/courses
+
+const courses = [ 
+  {
+    id: 1,
+    courseId: 1,
+    title: 'javascript는 왜 이모양인가',
+    pages: [
+      { pageId: 1, title: '배열' },
+      { pageId: 2, title: '객체' }
+    ]
+  },
+  {
+    id: 2,
+    courseId: 2,
+    title: 'typescript를 왜 선택했는가',
+    pages: [
+      { pageId: 3, title: '타입선언' },
+      { pageId: 4, title: '인터페이스' }
+    ]
+  }
+];
+
+export default courses;
