eb7be3985cdfd225ee8b67ad4be867a30fc5361b
ParkDongJo <pdj10211@gmail.com>
2020-12-05 04:25:26 +0900

    [week3-task2] Drawer 위치이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
 import React from 'react';
 
-import { useDispatch } from 'react-redux';
-
 import {
   Switch,
   Route,
-  useLocation,
 } from 'react-router-dom';
 
 import Header from './Header';
-import Drawer from './Drawer';
 import Footer from './Footer';
 import HomePage from './HomePage'
 import BookPage from './BookPage'
 import IntroPage from './IntroPage'
 
 export default function App() {
-  const location = useLocation();
-
-  function isShowDrawer() {
-    return /\/book/g.test(location.pathname);
-  }
 
   return (
     <>
@@ -30,7 +21,7 @@ export default function App() {
         <Route path="/intro" component={IntroPage} />
         <Route path="/books/:bookId" component={BookPage} />
       </Switch>
-      <Drawer isOpen={isShowDrawer()} />
+      
 
       <Footer />
     </>
--- src/BookPage.jsx
+++ src/BookPage.jsx
@@ -6,25 +6,35 @@ import { useParams } from 'react-router-dom';
 
 import BookContainer from './BookContainer';
 
+import DrawerContainer from './DrawerContainer';
+
 import {
   loadCourse,
   loadPage,
 } from './store/bookSlice';
 
-export default function BookPage({ params }) {
-  const { bookId } = params || useParams();
+const FIRST_OPENED_COURSE_ID = 1;
+const FIRST_OPENED_PAGE_ID = 1;
 
+export default function BookPage({ params }) {
   const dispatch = useDispatch();
+
+  const { bookId } = params || useParams();
 
   useEffect(() => {
     dispatch(loadCourse({ bookId }));
-    dispatch(loadPage({ bookId, courseId: 1, pageId: 1 }));
+    dispatch(loadPage({
+      bookId,
+      courseId: FIRST_OPENED_COURSE_ID,
+      pageId: FIRST_OPENED_PAGE_ID
+    }));
   }, []);
 
   return (
     <>
       <h1>Book</h1>
       <BookContainer />
+      <DrawerContainer />
     </>
   );
 };
