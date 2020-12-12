1ce290f6879fc829af7af6db11a383b007203323
ParkDongJo <pdj10211@gmail.com>
2020-12-12 22:37:02 +0900

    [week4-task1] react-markdown 사용 & page에서 title 추출

--- src/BookContainer.test.jsx
+++ src/BookContainer.test.jsx
@@ -10,9 +10,10 @@ import { page1 } from './../fixtures';
 
 describe('BookContainer', () => {
   const dispatch = jest.fn();
+  const extractSubTitlesInPage = jest.fn();
 
   beforeEach(() => {
-    dispatch.mockClear();
+    jest.clearAllMocks();
     useDispatch.mockImplementation(() => dispatch);
 
     useSelector.mockImplementation((selector) => selector({
@@ -20,6 +21,15 @@ describe('BookContainer', () => {
         opendPage: page1
       }
     }));
+
+    extractSubTitlesInPage.mockImplementation((pageText) => {
+      const titles = pageText.match(/##\D+?\n/gm);
+
+      return titles.map(title => {
+        return title.replace(/(##\s|##)/g, '')
+                    .replace(/\n/g, '');
+      });
+    });
   });
 
   function renderBookContainer() {
@@ -29,22 +39,29 @@ describe('BookContainer', () => {
   };
 
   context('when render book container', () => {
-    it('check book page text', () => {
-      const { container, getByText, queryByText } = renderBookContainer();
+    it('show text of book page', () => {
+      const { getByText, queryByText } = renderBookContainer();
 
-      expect(container).toHaveTextContent('BookContainer');
-      
       expect(getByText('시작하면서..')).not.toBeNull();
-      // expect(queryByText('그래서?')).not.toBeNull();
-      // expect(queryByText('일단 기다려봅시다.')).not.toBeNull();
+      expect(queryByText('그래서?')).not.toBeNull();
+      expect(queryByText('일단 기다려봅시다')).not.toBeNull();
     });
 
-    it('check container layout style', () => {
+    it('show layout style of container', () => {
       const { getByTestId } = renderBookContainer();
       const element = getByTestId('book-layout')
   
       expect(element).toHaveStyle('width: 768px');
       expect(element).toHaveStyle('margin: 0 auto');
+    });
+  });
+
+  context('when run extractSubTitleInPage()', () => {
+    it('return matchedSubTitles',() => {
+      const subTitlesInPage = extractSubTitlesInPage(page1.text);
+
+      expect(subTitlesInPage).not.toBeNull();
+      expect(subTitlesInPage[0]).toBe('일단 기다려봅시다')
     });
   });
 });
