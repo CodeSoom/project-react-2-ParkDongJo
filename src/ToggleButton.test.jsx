fb2fa37eb67753c5667eaecaf0977804695324dd
ParkDongJo <pdj10211@gmail.com>
2020-12-23 23:33:36 +0900

    [week6][etc] 토글버튼 테스트 코드 작성

--- src/ToggleButton.test.jsx
+++ src/ToggleButton.test.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import ToggleButton from './ToggleButton';
 
 describe('ToggleButton', () => {
-  function renderToggleButton() {
-    return render(<ToggleButton />)
+  const toggleSelected = jest.fn();
+
+  beforeEach(() => {
+    toggleSelected.mockClear();
+  });
+
+  function renderToggleButton({ selected }) {
+    return render(
+      <ToggleButton 
+        isSelected={selected}
+        toggleSelected={toggleSelected}
+      />
+    )
   }
 
-  it('render component', () => {
-    const { getByText } = renderToggleButton();
+  context('when selected is true', () => {
+    it('render ON text', () => {
+      const { getByText } = renderToggleButton({ selected: false });
 
-    expect(getByText("ON")).not.toBeNull();
+      expect(getByText("ON")).not.toBeNull();
+    });
+
+    it('click toggleSelected', () => {
+      const { getByTestId } = renderToggleButton({ selected: false });
+
+      fireEvent.click(getByTestId('toggle-button'));
+
+      expect(toggleSelected).toBeCalled();
+    });
+  });
+
+  context('when selected is false', () => {
+    it('render OFF text', () => {
+      const { getByText } = renderToggleButton({ selected: true });
+
+      expect(getByText("OFF")).not.toBeNull();
+    });
   });
 });
