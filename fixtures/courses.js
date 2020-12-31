76eebbea41d8b0cd2b88b8dd216ddf6e0f570383
ParkDongJo <pdj10211@gmail.com>
2020-12-31 22:53:38 +0900

    [week7] 실제 api로 교체

--- fixtures/courses.js
+++ fixtures/courses.js
@@ -6,20 +6,14 @@ const courses = [
     bookId: 1,
     courseId: 1,
     title: 'javascript는 왜 이모양인가',
-    pages: [
-      { pageId: 1, title: '배열' },
-      { pageId: 2, title: '객체' }
-    ]
+    pages: '[{ "pageId": 1, "title": "배열" }, { "pageId": 2, "title": "객체" }]'
   },
   {
     id: 2,
     bookId: 1,
     courseId: 2,
     title: 'typescript를 왜 선택했는가',
-    pages: [
-      { pageId: 3, title: '타입선언' },
-      { pageId: 4, title: '인터페이스' }
-    ]
+    pages: '[{ "pageId": 3, "title": "타입선언" },{ "pageId": 4, "title": "인터페이스" }]'
   }
 ];
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,14 @@
-import axios from './axios';
+// import axios from './axios';
+import axios from 'axios';
 
 export async function fetchCourses({ bookId }) {
-  const url = `https://api.com/books/${bookId}/courses`;
+  const url = `https://ws0feiccr7.execute-api.ap-northeast-2.amazonaws.com/dev/api/books/${bookId}/courses`;
   const response = await axios.get(url);
   return response.data;
 }
 
 export async function fetchPage({ bookId, courseId, pageId }) {
-  const url = `https://api.com/books/${bookId}/courses/${courseId}/pages/${pageId}`;
+  const url = `https://ws0feiccr7.execute-api.ap-northeast-2.amazonaws.com/dev/api/books/${bookId}/courses/${courseId}/pages/${pageId}`;
   const response = await axios.get(url);
   return response.data;
 }
